409ef868cc982257bb63c1d5f486322444537ea2
moizusmancs <moizusmancs@gmail.com>
2026-02-23 02:37:39 +0500

    manual struct initiliztion in main file

--- 2/main.go
+++ 2/main.go
@@ -1,7 +1,16 @@
 package main
 
+// import (
+// 	"bufio"
+// 	"os"
+// )
+
 func main(){
 	// playTodo()
 	app := NewTodoApp()
 	app.Run()
+
+	// var app TodoApp
+	// app.tasks = []string{}
+	// app.scanner = bufio.NewScanner(os.Stdin)
 }
